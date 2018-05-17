de1e1cb41718f266ffe590a3d6a6b40480ca0bba
ahotko <ales.hotko@zejn.si>
2018-05-17 15:01:06 +0200

    Small Additions

---
+++ SimpleQueryBuilder/SimpleQueryBuilder/Builder.Where.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQueryBuilder
+{
+    public partial class Builder
+    {
+        private Collection<Tuple<string, string, object>> _conditions = new Collection<Tuple<string, string, object>>();
+
+        public Builder Where(string fieldName, object condition)
+        {
+            return Where(fieldName, "=", condition);
+        }
+
+        public Builder Where(string fieldName, string conditionType, object condition)
+        {
+            var _cond = new Tuple<string, string, object>(fieldName, conditionType, condition);
+            _conditions.Add(_cond);
+            return this;
+        }
+    }
+}
--- SimpleQueryBuilder/SimpleQueryBuilder/Builder.cs
+++ SimpleQueryBuilder/SimpleQueryBuilder/Builder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,21 @@ namespace SimpleQueryBuilder
 {
     public partial class Builder
     {
+        private string _tableName = String.Empty;
+        private string _tableAlias = String.Empty;
+        private Collection<string> _fieldNames = null;
 
         public Builder Query(string tableName)
         {
+            _tableName = tableName.ToUpper();
+            _tableAlias = tableName.Substring(0, 1).ToLower();
             return this;
         }
 
         public Builder Query(string tableName, string alias)
         {
+            _tableName = tableName.ToUpper();
+            _tableAlias = alias.ToLower();
             return this;
         }
     }
