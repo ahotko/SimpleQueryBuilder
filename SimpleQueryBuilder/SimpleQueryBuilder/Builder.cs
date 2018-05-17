c4311e353f4d8fca9553520340701ef39a7c22c4
ahotko <ales.hotko@zejn.si>
2018-05-17 10:37:58 +0200

    First Version

---
+++ SimpleQueryBuilder/SimpleQueryBuilder/Builder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQueryBuilder
+{
+    public partial class Builder
+    {
+
+        public Builder Query(string tableName)
+        {
+            return this;
+        }
+
+        public Builder Query(string tableName, string alias)
+        {
+            return this;
+        }
+    }
+}
